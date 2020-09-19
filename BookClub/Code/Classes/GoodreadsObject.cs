b02dfacaf4dc68e8ac7f988d6c8dddab88c0833c
Azad Nalbandian <azad@jatech.co.uk>
2020-09-19 11:47:29 +0100

    Initial commit

---
+++ BookClub/Code/Classes/GoodreadsObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Configuration;
+
+namespace BookClub.Code.Classes
+{
+    public class GoodreadsObject
+    {
+        static readonly HttpClient client = new HttpClient();
+        private readonly string key = ConfigurationManager.AppSettings["apiKey"];
+        private readonly string secret = ConfigurationManager.AppSettings["apiSecret"];
+        const string shelf = "books-i-own";
+
+        public int id { get; set; }
+
+        public GoodreadsObject(int ID)
+        {
+            id = ID;
+        }
+
+        public async Task<string> GetBookToRead ()
+        {
+            XDocument xml = new XDocument();
+
+            try
+            {
+                string uri = ($"https://www.goodreads.com/review/list?v=2&key={key}&id={id}&shelf={shelf}&sort=random&per_page=200");
+                var result = await client.GetAsync(uri);
+                string shelves = await result.Content.ReadAsStringAsync();
+                xml = XDocument.Parse(shelves);
+                string book = xml.Elements().Descendants("book").Elements("title").Select(x => x.Value.ToString()).First();
+
+                return book;
+            }
+            catch (Exception e)
+            {
+                return string.Empty;
+            }
+        }
+    }
+}
---
+++ BookClub/Program.cs
@@ -0,0 +1,47 @@
+﻿using BookClub.Code.Classes;
+using System;
+using System.Threading.Tasks;
+
+namespace BookClub
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int id = IDEntry();
+
+            string book = await GetGoodReadsAsync(id);
+            while (book == string.Empty)
+            {
+                Console.WriteLine("Looks like there was an issue. Please try again");
+                IDEntry();
+            }
+
+            Console.WriteLine($"Your chosen book is { book }! Press any key to quit.");
+            Console.Read();
+        }
+
+        static int IDEntry ()
+        {
+            Console.WriteLine("Please enter your Goodreads user ID.");
+            int id;
+
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid ID, please try again.");
+            }
+
+            return id;
+        }
+
+        static async Task<string> GetGoodReadsAsync(int id)
+        {
+            GoodreadsObject api = new GoodreadsObject(id);
+
+            Console.WriteLine("Picking a book...");
+            string result = await api.GetBookToRead();
+            return result;
+        }
+    }
+}
